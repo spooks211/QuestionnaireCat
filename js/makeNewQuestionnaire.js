ee169f17bf12673bac6e79be82d4077c80d5f888
spooks211 <noah.povil@gmail.com>
2020-06-06 18:29:25 +0100

    first commit

---
+++ js/makeNewQuestionnaire.js
@@ -0,0 +1,71 @@
+"use strict";
+
+let newAnswerUniqueIdCounter = 3; 
+//this starts at 3 because 2 answers are given by default, this is made
+//so each newly generated element can have a unique id
+
+//makes an array for all the instances of the element "removeButton"
+const removeButtonArray = [document.getElementById("removeButton1")];
+removeButtonArray.push(document.getElementById("removeButton2"));
+
+//Method that adds a new answer input field and new remove answer button
+function addNewAnswer(){
+    const newAnswer = document.createElement('input');
+    const newSection = document.createElement('section');
+    const newRemoveButton = document.createElement('input');
+
+    newSection.id = "AnswerSection" + newAnswerUniqueIdCounter;
+
+    newAnswer.className = "inputField";
+    newAnswer.type = "text";
+    newAnswer.placeholder = "Enter an answer";
+
+    newRemoveButton.className = "inputField";
+    newRemoveButton.type = "button";
+    newRemoveButton.value = "Remove answer";
+    newRemoveButton.id = "removeButton" + newAnswerUniqueIdCounter;
+    removeButtonArray.push(newRemoveButton);
+
+    //the onclick listener is made here rather than at the bottom of the code
+    //so the newly made remove buttons get added to and selected from the array
+    //properly
+    for (let i = 0; i < removeButtonArray.length; i++){
+        removeButtonArray[i].addEventListener('click', removeAnswer(i));     
+    }
+
+    newSection.appendChild(newAnswer);
+    newSection.appendChild(newRemoveButton);
+
+    const answerContainer = document.getElementById("QandASection");
+    answerContainer.appendChild(newSection);
+}
+
+
+//removes the parent element (AnswerSection) of whichever button is clicked,
+//is called from addNewAnswer()
+function removeAnswer(i){
+    return function(){
+        removeButtonArray[i].parentElement.remove();
+    }
+}
+
+//Method that adds the question and answer choices to questionnaire
+function addToQuestionnaire (){
+
+}
+
+//Onclick listener for add new answer button
+const addAnswerButton = document.getElementById("addNewAnswer");
+
+addAnswerButton.addEventListener('click', event => {
+    addNewAnswer();
+    newAnswerUniqueIdCounter += 1;
+
+});
+
+//Onclick listener for add to questionnaire button
+const addQuestionButton = document.getElementById("addNewQuestionSubmitButton");
+
+addQuestionButton.addEventListener('click', event => {
+    addToQuestionnaire();
+});
