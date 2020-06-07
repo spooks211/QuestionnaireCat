615929a0ee310ed261ab2191e74d598b184c9ac7
Spooky232 <44676349+Spooky232@users.noreply.github.com>
2020-06-07 19:36:12 +0100

    added body-parser library and more code in server.js

--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
 "use strict";
 
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const parser = bodyparser();
+
+app.use('/', express.static('client'));
+
